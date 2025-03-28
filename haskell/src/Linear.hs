67076e6d2aaf8f549fe54ea237bf1a96da7a6f7e
kscarrot <kelstrait@icloud.com>
2025-03-28 17:41:19 +0800

    feat: 迁移到haskell 更新测试

--- haskell/src/Linear.hs
+++ haskell/src/Linear.hs
@@ -1,18 +1,27 @@
-module Linear (
-    Scalar(..),
+module Linear
+  ( Scalar (..),
     Vector,
     Matrix,
     标量加,
     标量乘,
     向量加,
-    向量乘
-) where
+    向量乘,
+    标量乘向量,
+    转置,
+    矩阵乘,
+    标量乘矩阵,
+    矩阵加,
+    向量乘矩阵,
+  )
+where
 
-data Scalar = Str String
-          | Num Double
-          deriving (Show, Eq)
+data Scalar
+  = Str String
+  | Num Double
+  deriving (Show, Eq)
 
 type Vector = [Scalar]
+
 type Matrix = [Vector]
 
 标量加 :: Scalar -> Scalar -> Scalar
@@ -30,12 +39,33 @@ type Matrix = [Vector]
 标量乘 _ _ = error "不支持的类型"
 
 向量加 :: Vector -> Vector -> Vector
-向量加 x y
-    | length x /= length y = error "向量长度不匹配"
-    | otherwise = zipWith 标量加 x y
+向量加 = zipWith 标量加
 
 向量乘 :: Vector -> Vector -> Scalar
-向量乘 x y
-    | length x /= length y = error "向量长度不匹配"
-    | otherwise = foldr1 标量加 $ zipWith 标量乘 x y
+向量乘 x y = foldr1 标量加 $ zipWith 标量乘 x y
 
+-- 标量乘向量
+标量乘向量 :: Scalar -> Vector -> Vector
+标量乘向量 s = map (标量乘 s)
+
+-- 矩阵转置
+转置 :: Matrix -> Matrix
+转置 [] = []
+转置 ([] : _) = []
+转置 m = map head m : 转置 (map tail m)
+
+-- 矩阵乘法
+矩阵乘 :: Matrix -> Matrix -> Matrix
+矩阵乘 a b = [[ 行 `向量乘`列 | 列 <- 转置 b] | 行 <- a]
+
+-- 标量乘矩阵
+标量乘矩阵 :: Scalar -> Matrix -> Matrix
+标量乘矩阵 s = map (标量乘向量 s)
+
+-- 矩阵加法
+矩阵加 :: Matrix -> Matrix -> Matrix
+矩阵加 = zipWith 向量加
+
+-- 向量乘矩阵
+向量乘矩阵 :: Vector -> Matrix -> Vector
+向量乘矩阵 向量 m = [向量  `向量乘`  列 | 列 <- 转置 m]
--- haskell/test/LinearSpec.hs
+++ haskell/test/LinearSpec.hs
@@ -1,77 +1,82 @@
 module LinearSpec (spec) where
 
-import Test.Hspec
-import Control.Exception (evaluate)
 import Linear
+import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "标量运算" $ do
-    describe "标量加" $ do
-      it "浮点数相加应该返回正确的和" $ do
-        Num 1.5 `标量加` Num 2.5 `shouldBe` Num 4.0
+    it "数字加法" $
+      Num 1.0 `标量加` Num 2.0 `shouldBe` Num 3.0
 
-      it "浮点数相加兼容整形 返回浮点数" $ do
-        Num 5.0 `标量加` Num 5 `shouldBe` Num 10.0
+    it "字符串加法" $
+      Str "hello" `标量加` Str "world" `shouldBe` Str "hello+world"
 
-      it "整形数相加应该返回正确的浮点数结果" $ do
-        Num 1 `标量加` Num 2 `shouldBe` Num 3.0
+    it "空字符串加法" $ do
+      Str "" `标量加` Str "test" `shouldBe` Str "test"
+      Str "test" `标量加` Str "" `shouldBe` Str "test"
 
-      it "字符串相加应该返回正确的连接结果" $ do
-        Str "Hello" `标量加` Str "World" `shouldBe` Str "Hello+World"
+    it "数字乘法" $
+      Num 2.0 `标量乘` Num 3.0 `shouldBe` Num 6.0
 
-      it "字符串相加空串处理" $ do
-        Str "" `标量加` Str "World" `shouldBe` Str "World"
-        Str "" `标量加` Str "" `shouldBe` Str ""
+    it "字符串乘法" $
+      Str "hello" `标量乘` Str "world" `shouldBe` Str "helloworld"
 
-      it "不同类型相加应该抛出异常" $ do
-        evaluate (Num 1.5 `标量加` Str "World") `shouldThrow` anyException
-
-    describe "标量乘" $ do
-      it "浮点数相乘应该返回正确的积" $ do
-        Num 1.5 `标量乘` Num 2.0 `shouldBe` Num 3.0
-
-      it "字符串相乘应该返回正确的连接结果" $ do
-        Str "Hello" `标量乘` Str "World" `shouldBe` Str "HelloWorld"
-
-      it "空字符串相乘应该返回空字符串" $ do
-        Str "" `标量乘` Str "World" `shouldBe` Str ""
-
-      it "不同类型相乘应该抛出异常" $ do
-        evaluate (Num 1.5 `标量乘` Str "World") `shouldThrow` anyException
+    it "空字符串乘法" $ do
+      Str "" `标量乘` Str "test" `shouldBe` Str ""
+      Str "test" `标量乘` Str "" `shouldBe` Str ""
 
   describe "向量运算" $ do
-    describe "向量加" $ do
-      it "向量相加应该返回正确的和" $ do
-        let x = [Num 1.0, Num 2.0, Num 3.0]
-            y = [Num 4.0, Num 5.0, Num 6.0]
-            result = x `向量加` y
-        result `shouldBe` [Num 5.0, Num 7.0, Num 9.0]
+    let v1 = [Num 1.0, Num 2.0]
+    let v2 = [Num 3.0, Num 4.0]
+    let sv1 = [Str "a", Str "b"]
+    let sv2 = [Str "c", Str "d"]
 
-      it "不同长度向量相加应该抛出异常" $ do
-        let x = [Num 1.0, Num 2.0]
-            y = [Num 3.0, Num 4.0, Num 5.0]
-        evaluate (x `向量加` y) `shouldThrow` anyException
+    it "数字向量加法" $
+      v1 `向量加` v2 `shouldBe` [Num 4.0, Num 6.0]
 
-      it "字符串向量相加应该返回正确的连接结果" $ do
-        let x = [Str "Hello", Str "World"]
-            y = [Str " ", Str "!"]
-            result = x `向量加` y
-        result `shouldBe` [Str "Hello+ ", Str "World+!"]
+    it "字符串向量加法" $
+      sv1 `向量加` sv2 `shouldBe` [Str "a+c", Str "b+d"]
 
-    describe "向量乘" $ do
-      it "向量相乘应该返回正确的元素积" $ do
-        let x = [Num 1.0, Num 2.0, Num 3.0]
-            y = [Num 4.0, Num 5.0, Num 6.0]
-            -- 1⋅4+2⋅5+3⋅6 = 32
-        x `向量乘` y `shouldBe` Num 32.0
+    it "数字向量点积" $
+      v1 `向量乘` v2 `shouldBe` Num 11.0
 
-      it "字符串向量相乘应该返回正确的连接结果" $ do
-        let x = [Str "x_1", Str "y_1", Str "z_1"]
-            y = [Str "x_2", Str "y_2", Str "z_2"]
-        x `向量乘` y `shouldBe` Str "x_1x_2+y_1y_2+z_1z_2"
+    it "字符串向量点积" $
+      sv1 `向量乘` sv2 `shouldBe` Str "ac+bd"
 
-      it "不同长度向量相乘应该抛出异常" $ do
-        let x = [Num 1.0, Num 2.0]
-            y = [Num 3.0, Num 4.0, Num 5.0]
-        evaluate (x `向量乘` y) `shouldThrow` anyException 
+    it "标量乘数字向量" $
+      Num 2.0 `标量乘向量` v1 `shouldBe` [Num 2.0, Num 4.0]
+
+    it "标量乘字符串向量" $
+      Str "x" `标量乘向量` sv1 `shouldBe` [Str "xa", Str "xb"]
+
+  describe "矩阵运算" $ do
+    let m1 = [[Num 1.0, Num 2.0], [Num 3.0, Num 4.0]]
+    let m2 = [[Num 5.0, Num 6.0], [Num 7.0, Num 8.0]]
+    let v1 = [Num 1.0, Num 2.0]
+
+    it "矩阵转置" $
+      转置 m1 `shouldBe` [[Num 1.0, Num 3.0], [Num 2.0, Num 4.0]]
+
+    it "矩阵乘法" $
+      m1 `矩阵乘` m2 `shouldBe` [[Num 19.0, Num 22.0], [Num 43.0, Num 50.0]]
+
+    it "标量乘矩阵" $
+      Num 2.0 `标量乘矩阵` m1 `shouldBe` [[Num 2.0, Num 4.0], [Num 6.0, Num 8.0]]
+
+    it "矩阵加法" $
+      m1 `矩阵加` m2 `shouldBe` [[Num 6.0, Num 8.0], [Num 10.0, Num 12.0]]
+
+    it "向量乘矩阵" $
+      v1 `向量乘矩阵` m1 `shouldBe` [Num 7.0, Num 10.0]
+
+  describe "空矩阵和向量处理" $ do
+    it "空矩阵转置" $
+      转置 [] `shouldBe` []
+
+  describe "字符串矩阵乘法" $ do
+    let m1 = [[Str "a", Str "b"], [Str "c", Str "d"]]
+    let m2 = [[Str "e", Str "f"], [Str "g", Str "h"]]
+
+    it "字符串矩阵乘法" $
+      m1 `矩阵乘` m2 `shouldBe` [[Str "ae+bg", Str "af+bh"], [Str "ce+dg", Str "cf+dh"]]
